dfdf0686794f2ef8deb670c76f54529fb5b37218
long.chu <long.chu@savvycomsoftware.com>
2023-05-29 14:52:32 +0700

    feat: apply express

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,51 @@
-import * as http from "http";
-import * as dotenv from "dotenv";
-import * as fs from "fs";
-dotenv.config();
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
 
 const port = process.env.PORT;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader("Access-Control-Max-Age", 2592000); // 30 days
-  if (
-    (req.url === "/api/todos") &&
-    req.method === "GET"
-  ) {
-    res.end(
-      JSON.stringify([
-        {
-          userId: 1,
-          id: 1,
-          title: "data test 1",
-          completed: false,
-        },
-        {
-          userId: 1,
-          id: 2,
-          title: "data test 2",
-          completed: false,
-        },
-      ])
-    );
-  } else if (req.url === "/tcs" && req.method === "GET") {
-    res.end("HI TSC");
-  } else {
-    res.statusCode = 404;
-    res.end("404: Not Found");
-  }
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-server.listen(port, function () {
+app.get("/api/todos", (req, res) => {
+  const page = req.query.page;
+  const limit = req.query.limit;
+
+  // handle data with pagination here
+  console.log(page, limit);
+
+  res.json([
+    {
+      userId: 1,
+      id: 1,
+      title: "data test 1",
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: "data test 2",
+      completed: false,
+    },
+  ]);
+});
+
+app.post("/api/todos", (req, res) => {
+  const userId = req.body.userId;
+  const title = req.body.title;
+  const completed = req.body.completed;
+
+  // handle create new task here
+  console.log(userId, title, completed)
+
+  res.send({data: true});
+});
+
+app.listen(port, function () {
   console.log("Server running at http://127.0.0.1" + ":" + port + "/");
 });
-
-// list method GET 5 first
-// create method POST
